66385573dbcea5b9f2de4f65ea9550859a76a330
ivanaica <38052523+ivanaica@users.noreply.github.com>
2024-05-08 11:41:59 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    baseUrl: 'https://dashboard.helloagain.at/#/',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
   },
 });
